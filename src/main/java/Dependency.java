5b45d30c949712a0a4440f5589ba58be97541a1a
Alex <alexandre_2420@outlook.com>
2022-03-18 08:02:28 -0500

    add: Dependency and SubDependency classes

---
+++ src/main/java/Dependency.java
@@ -0,0 +1,23 @@
+public class Dependency {
+    private final SubDependency subDependency;
+
+    public Dependency(SubDependency subDependency) {
+        this.subDependency = subDependency;
+    }
+
+    public String getClassName() {
+        return this.getClass().getSimpleName();
+    }
+
+    public String getSubdepedencyClassName() {
+        return subDependency.getClassName();
+    }
+
+    public int addTwo(int i) {
+        return i + 2;
+    }
+
+    public String getClassNameUpperCase() {
+        return getClassName().toUpperCase();
+    }
+}
