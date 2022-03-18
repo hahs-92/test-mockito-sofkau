75de8f69a8c3a59b4fe0fc04f1d33c672f481a54
Alex <alexandre_2420@outlook.com>
2022-03-18 14:04:48 -0500

    ready to delivery

--- src/test/java/DependencyVerifyTest.java
+++ src/test/java/DependencyVerifyTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
